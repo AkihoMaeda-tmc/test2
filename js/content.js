46271137df7e8ee16fd63af1bd06093c47bce52a
AkihoMaeda-tmc <98142792+AkihoMaeda-tmc@users.noreply.github.com>
2022-01-21 17:36:07 +0900

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,4 +1,5 @@
 const images = [
+　{src: 'scorpio.svg', width: 267.02, height: 218.31},
   {src: 'img/Asset_1.svg', width: 267.02, height: 218.31},
   {src: 'img/Asset_11.svg', width: 204.36, height: 375.54},
   {src: 'img/Asset_12.svg', width: 252.37, height: 254.81},
