f4850ca13bf4d5c2ecc9bd123ed5d29cc97734e9
AkihoMaeda-tmc <98142792+AkihoMaeda-tmc@users.noreply.github.com>
2022-01-24 13:56:25 +0900

    Update content.js

--- js/content.js
+++ js/content.js
@@ -51,7 +51,7 @@ const images = [
 
 function getImage(img) {
   return `<div class="draggable-item image-box">
-						<img src="${img.src}" data-image-url="https://realtimeboard.com/api/awesome-plugins/plugins/DragDrop/${img.src}">
+						<img src="${img.src}" data-image-url="https://github.com/AkihoMaeda-tmc/test2/${img.src}">
 			</div>`
 }
 
