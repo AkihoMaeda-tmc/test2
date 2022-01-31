c5e5fea10bf5b1dd64e17d7dc20c30c5bb30d596
AkihoMaeda-tmc <98142792+AkihoMaeda-tmc@users.noreply.github.com>
2022-01-31 14:30:36 +0900

    Update content.js

--- js/content.js
+++ js/content.js
@@ -24,10 +24,10 @@ function getImage(img) {
 function addShapes(container) {
 //   container.innerHTML += `<div class="shape draggable-item green" data-color="0ca788">I am shape</div>
 //   				<div class="shape draggable-item red" data-color="f24726">Me too</div>
-     container.innerHTML += `<div class="draggable-item">
+     container.innerHTML += `<div class=" shape draggable-item">
      				<div class="arrow"></div>
 			  </div>
-			  <div class="draggable-item">
+			  <div class="shape draggable-item">
      				<div class="koutei"></div>
 			  </div>`
 			     
